f137186c530cf95fd57da5d1822cf92d8f0f8133
Nadia Khan <nadia.khan@sky.uk>
2019-08-12 08:41:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,57 +1,3 @@
-WIN_COMBINATIONS = [
-  [0,1,2], 
-  [3,4,5], 
-  [6,7,8], 
-  [0,3,6], 
-  [1,4,7], 
-  [2,5,8], 
-  [0,4,8], 
-  [6,4,2]
-]
-
-def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-    position_1 = board[win_index_1] 
-    position_2 = board[win_index_2] 
-    position_3 = board[win_index_3] 
-    if (position_1 =="X" && position_2 == "X" && position_3 == "X") or (position_1 == 'O' && position_2 == "O" && position_3 == "O") then
-      return win_combination
-    else
-      false
-    end
-  end
-  false
-end
-
-def full?(board)
-  board.each { |crosses|
-    if crosses == " "
-      return false
-    end      
-  }
-    
-  return true 
-end 
-
-def draw?(board)
-  !won?(board) && full?(board)
-end
-
-def over?(board)
-   if draw?(board) || won?(board) 
-     return true
-   end
-end  
-
-def winner(board)
-   if won?(board)
-      return board[won?(board)[0]]
-   end
-end
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -59,54 +5,133 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-board = [" "," "," "," "," X "," "," "," "," "]
 
+# input_to_index
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+# move
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+# position_taken
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+# valid_move
 def valid_move?(board, index)
-   if index.between?(0,8) && !position_taken?(board, index) then
-     return true
-   else
-     return false
-   end
-end 
-
-def position_taken?(board, index)
-   if board [index] == " " 
-     return false
-   end
-     return true
-  
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
-def current_player(board)
-   turn_count(board) % 2 == 0 ? "X" : "O"
-end
-
-def turn_count(board)
-    counter = 0
-   board.each do |spaces|
-      if spaces == "X" || spaces == "O"
-         counter += 1
-      end
-   end
-   counter
-end
-
-def current_player(board)
-   turn_count(board) % 2 == 0 ? "X" : "O"
-end
-
-def input_to_index(input)
-  input.to_i - 1 
-end  
-
-def move(board, index, character)
-  board [index] =character
-end  
-  
+# turn
 def turn(board)
   puts "Please enter 1-9:"
-  i = gets.strip
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
   end
+end
+
+# turn_count
+def turn_count(board)
+  turn_count = 0
+
+  board.each do |counter|
+    if counter == "X" || counter == "O"
+      turn_count += 1
+    end
+  end
+
+  turn_count
+end
+
+# current_player
+def current_player(board)
+  turn_count(board).even? ? "X" : turn_count(board).odd? ? "O" : "X"
+end
+
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [0,3,6],
+  [0,4,8],
+  [1,4,7],
+  [2,5,8],
+  [6,4,2],
+  [3,4,5],
+  [6,7,8]
+]
+
+# won
+def won?(board)
+
+  WIN_COMBINATIONS.each do |win_combo|
+    p1 = board[win_combo[0]]
+    p2 = board[win_combo[1]]
+    p3 = board[win_combo[2]]
+
+    if (p1 == "X" && p2 == "X" && p3 == "X") || (p1 == "O" && p2 == "O" && p3 == "O")
+      return win_combo
+    end
+
+  end
+    return false
+end
+
+# full
+def full?(board)
+  board.all? { |i| i == "X" || i == "O" }
+end
+
+# draw
+def draw?(board)
+  if (full?(board) == false)
+    return false
+  else
+    if (won?(board) != false)
+      return false
+    else
+      return true
+    end
+  end
+end
+
+# over
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+# winner
+def winner(board)
+  if (over?(board))
+    if (board[won?(board)[0]] == "X")
+      return "X"
+    else
+      return "O"
+    end
+  else
+  end
+end
+
+# Define your play method below
+def play(board)
+
+  while !over?(board)
+    turn(board)
+  end
+  if won?(board)
+    return puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    return puts "Cats Game!"
+  end
+
 end
 
 

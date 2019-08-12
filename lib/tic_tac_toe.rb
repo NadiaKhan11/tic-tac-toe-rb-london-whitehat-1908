5aba343087f63fb50b7c4358d46cc5599674eb93
Nadia Khan <nadia.khan@sky.uk>
2019-08-12 08:26:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -103,7 +103,15 @@ def move(board, index, character)
   board [index] =character
 end  
   
-def turn
-end  
+def turn(board)
+   puts "Please enter 1-9:"
+   user_input = gets.strip
+   index = input_to_index(user_input)
+   if valid_move?(board, index)
+      move(board, index, current_player(board))
+      turn(board)
+   end
+   display_board(board)
+end 
 
 
